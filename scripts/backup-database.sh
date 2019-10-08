640506164c30a73a2c5cfdabf76d602c04a808d1
ich777 <ich777@noreply.example.org>
2019-10-08 21:08:47 +0200

    „scripts/backup-database.sh“ ändern

--- scripts/backup-database.sh
+++ scripts/backup-database.sh
@@ -1,6 +1,4 @@
 while true ; do
 	mysqldump -u csmm -p"csmm7dtd" 7dtd > $DATA_DIR/Database/7dtd-1.sql
 	sleep 60
-	mysqldump -u csmm -p"csmm7dtd" 7dtd > $DATA_DIR/Database/7dtd-2.sql
-	sleep 60
 done
